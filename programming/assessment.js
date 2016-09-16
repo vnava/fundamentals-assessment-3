610289296f662758b22eb497a18823ed11d6c191
Vanessa Nava <vnava26@gmail.com>
2016-09-15 20:11:15 -0700

    added assessment

--- programming/assessment.js
+++ programming/assessment.js
@@ -1,20 +1,26 @@
 //emptyFunc
 
+var emptyFunc = [];
+emptyFunc :
 
-
-
+emptyFunc();
 
 //myProfile
 
-
-
+var myProfile = [];
+name: "myname", 
+age: 27, 
+knowsJavascript = true;
+pets: ["kokis"]
 
 
 //printProfile
 
+var printProfile = function(myProfile)
 
+for(var = 0; i < myProfile.length; i++);
 
-
+printProfile(myProfile);
 
 //sumOf
 
