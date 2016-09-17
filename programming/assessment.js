6f74aa4104d2306989ed68b75953b979370e10cf
Vanessa Nava <vnava26@gmail.com>
2016-09-17 16:06:05 -0700

    added assessment

--- programming/assessment.js
+++ programming/assessment.js
@@ -1,19 +1,20 @@
 //emptyFunc
 
 var emptyFunc = [];
-emptyFunc :
+
+function emptyFunc() {}
 
 emptyFunc();
 
 //myProfile
 
-var myProfile = [];
+var myProfile = {
 name: "myname", 
 age: 27, 
-knowsJavascript = true;
+knowsJavascript: true,
 pets: ["kokis"]
 
-
+}
 //printProfile
 
 var printProfile = function(myProfile)
@@ -24,19 +25,40 @@ printProfile(myProfile);
 
 //sumOf
 
+var sumOf = [1, 2, 3, 4, 5];
+var total = 0;
 
+for(i=0; i < sumOf.length; i++){
+	total += array[i];
+}
+
+return total;
+
+};
+
+console.log sumOf([1,2,3,4,5]);
 
 
 
 //coinFlip
 
-
+function coinFlip() {
+	return Math.random(["heads", "tails"];
+}
 
 
 
 //everyOther
 
+var myArray = [1,'a',2,'b',3,'c',4,'d',5,'e'];
 
+for (var i = 0; i < myArray.length; i += 2) {
+
+newArray.push(myArray[i]);
+
+}  
+
+var newArray = everyOther(myArray);
 
 
 
